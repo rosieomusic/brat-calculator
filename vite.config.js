4475f43ea01af9f032cfc2943f9229a04dce4e5a
rosieomusic <rosieomusic@gmail.com>
2025-04-02 22:35:42 -0400

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -7,9 +5,4 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
 	base: '/brat-calculator/',
 	plugins: [vue()],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
-		},
-	},
 });
