e35a40a79c548d3d237f6c90e82b918e683c9487
rosieomusic <rosieomusic@gmail.com>
2025-04-02 23:47:13 -0400

    fixing vite config

---
+++ src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<script setup>
+	import CalculatorVue from '../components/CalculatorVue.vue';
+</script>
+
+<template>
+	<main>
+		<CalculatorVue />
+	</main>
+</template>
+
+<style>
+	body {
+		background-color: black;
+		display: flex;
+	}
+	main {
+		justify-content: center;
+	}
+	#app {
+		justify-content: center;
+	}
+</style>
