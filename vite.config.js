3ae09ab6280e890a0c4eb18e0205ba694810a90b
rosieomusic <rosieomusic@gmail.com>
2025-04-03 00:18:14 -0400

    ahhh

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	base: '/brat-calculator/',
+	base: 'brat-calculator/vue-project/',
 	plugins: [vue()],
 });
