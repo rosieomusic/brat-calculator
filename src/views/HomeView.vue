e25bcae93aec4444d4a6dfe616848f8583515de5
rosieomusic <rosieomusic@gmail.com>
2025-04-02 22:41:40 -0400

    fixed vite import issue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,22 @@
 <script setup>
-import CalculatorVue from '@/components/CalculatorVue.vue';
+	import CalculatorVue from '../components/CalculatorVue.vue';
 </script>
 
 <template>
-  <main>
-    <CalculatorVue />
-  </main>
+	<main>
+		<CalculatorVue />
+	</main>
 </template>
 
 <style>
-body{
-  background-color: black;
-  display: flex;
-}
-main{
-  justify-content: center;
-}
-#app{
-    justify-content: center;
-}
-
+	body {
+		background-color: black;
+		display: flex;
+	}
+	main {
+		justify-content: center;
+	}
+	#app {
+		justify-content: center;
+	}
 </style>
